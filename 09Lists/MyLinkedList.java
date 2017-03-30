59877ec14d1c8f46b5e4776e56d35c215c350543
JonahSW <jsachsw@gmail.com>
2017-03-30 13:56:24 -0400

    Began working on LinkedLists

---
+++ 09Lists/MyLinkedList.java
@@ -0,0 +1,50 @@
+public class LinkedList {
+
+    //Instance Variables
+    LNode start, end;
+
+    int size;
+
+    //Constructor for LinkedList
+    public LinkedList(){
+
+    }
+    
+    //Methods
+
+    //add method (adds to end)
+    public boolean add(int value){
+
+    }
+
+    //returns the size of the Linked List
+    public int size(){
+
+    }
+
+    //ToString method prints out the list
+    public String toString(){
+
+	System.out.println();
+    }
+
+    //--------------------------------------------------------------------------------------
+    //INNER CLASS FOR LNODE
+    private class LNode implements LinkedList{
+
+	//Constructor for node
+	private LNode(){
+
+	}
+
+	//Accessor Methods
+	
+    }
+    //END OF LNODE INNER CLASS
+    //--------------------------------------------------------------------------------------
+
+    //MAIN
+    public static void main(String[]arrgs){
+	
+    }
+}
