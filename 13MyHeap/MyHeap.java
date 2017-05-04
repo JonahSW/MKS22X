ccc0354cb12da47c7a4f527640f1d4d691ee5ee2
JonahSW <jsachsw@gmail.com>
2017-05-04 06:54:20 -0400

    Started the framewrk for the heap class

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyHeap{
+    //Constructs a heap of Strings data structure using an array
+    //Contains methods: add, remove, resize, peek(looks at top), push forward, push back, toString, resize
+
+    //VARIABLES
+    public Array
+    
+}
